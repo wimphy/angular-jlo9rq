a546b22e11770bbc16df4912a92e0d365434c1f5
wimphy <wimphy@126.com>
2020-09-02 17:33:46 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-jlo9rq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-jlo9rq)
---
+++ app/autocomplete-overview-example.ts
@@ -0,0 +1,70 @@
+import {Component} from '@angular/core';
+import {FormControl} from '@angular/forms';
+
+import {Observable} from 'rxjs/Observable';
+import {startWith} from 'rxjs/operators/startWith';
+import {map} from 'rxjs/operators/map';
+
+export class State {
+  constructor(public name: string, public population: string, public flag: string) { }
+}
+
+/**
+ * @title Autocomplete overview
+ */
+@Component({
+  selector: 'autocomplete-overview-example',
+  templateUrl: 'autocomplete-overview-example.html',
+  styleUrls: ['autocomplete-overview-example.css']
+})
+export class AutocompleteOverviewExample {
+  stateCtrl: FormControl;
+  filteredStates: Observable<any[]>;
+
+  states: State[] = [
+    {
+      name: 'Arkansas',
+      population: '2.978M',
+      // https://commons.wikimedia.org/wiki/File:Flag_of_Arkansas.svg
+      flag: 'https://upload.wikimedia.org/wikipedia/commons/9/9d/Flag_of_Arkansas.svg'
+    },
+    {
+      name: 'California',
+      population: '39.14M',
+      // https://commons.wikimedia.org/wiki/File:Flag_of_California.svg
+      flag: 'https://upload.wikimedia.org/wikipedia/commons/0/01/Flag_of_California.svg'
+    },
+    {
+      name: 'Florida',
+      population: '20.27M',
+      // https://commons.wikimedia.org/wiki/File:Flag_of_Florida.svg
+      flag: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Flag_of_Florida.svg'
+    },
+    {
+      name: 'Texas',
+      population: '27.47M',
+      // https://commons.wikimedia.org/wiki/File:Flag_of_Texas.svg
+      flag: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Flag_of_Texas.svg'
+    }
+  ];
+
+  constructor() {
+    this.stateCtrl = new FormControl();
+    this.filteredStates = this.stateCtrl.valueChanges
+      .pipe(
+        startWith(''),
+        map(state => state ? this.filterStates(state) : this.states.slice())
+      );
+  }
+
+  filterStates(name: string) {
+    return this.states.filter(state =>
+      state.name.toLowerCase().indexOf(name.toLowerCase()) === 0);
+  }
+
+}
+
+
+/**  Copyright 2017 Google Inc. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
